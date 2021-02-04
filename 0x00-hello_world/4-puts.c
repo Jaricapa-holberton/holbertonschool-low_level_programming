857f4ac6595f84dabd6916f3f502ae11f0cd7b72
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-03 19:13:49 -0500

    puts v1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main: prints exactly "Programming is like building a multilingual puzzle",
+ * followed by a new line.
+ *
+ * return: 
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
