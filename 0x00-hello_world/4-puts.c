4f9adbae32a6c8977d48639c2a774d1b4d20510b
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-03 19:37:15 -0500

    puts v6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/*
- * main: prints exactly "Programming is like building a multilingual puzzle",
- * followed by a new line.
+/**
+ * main - Entry point
  *
- * return: 
- *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -13,4 +11,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
