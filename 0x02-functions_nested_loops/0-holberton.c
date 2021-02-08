a9bcde27b65ec11f38809262ef0372cb7c04f168
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-08 17:42:55 -0500

    holberton v1

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - function main balblabla
+ * Return: return balblabva
+ */
+
+int main(void)
+{
+	int c;
+	char array[11] = "Holberton\n";
+
+	for (c = 0; c <= 11; c++)
+	{
+		_putchar(array[c]);
+	}
+	return (0);
+}
