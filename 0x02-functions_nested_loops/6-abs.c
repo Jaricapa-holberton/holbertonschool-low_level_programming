fcb53cddc661e581bb76173e8dc41cd32ad78e16
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-09 05:14:59 -0500

    abs v1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * n: get a number
+ * return: return a absolute value
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = a * -1;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
