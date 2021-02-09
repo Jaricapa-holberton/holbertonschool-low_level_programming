561f54fef4b55adb4ec59bb2d40e169e4f95e5b1
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-09 05:18:30 -0500

    header v1

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c); /* print text */
+void print_alphabet(void); /* print alphabet */
+void print_alphabet_x10(void); /* print alphabet x 10 */
+int _islower(int c); /* check lowercases */
+int _isalpha(int c); /* check alphabetic characters */
+int print_sign(int n); /* print the sing of a number */
+int _abs(int); /* computes the absolute value of an integer */
+int print_last_digit(int); /* prints the last digit of a number */
+void jack_bauer(void); /* prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59 */
+void times_table(void); /* prints the 9 times table, starting with 0 */
+int add(int, int); /* adds two integers and returns the result */
+void print_to_98(int n); /* prints all natural numbers from n to 98, followed by a new line */
+#endif
