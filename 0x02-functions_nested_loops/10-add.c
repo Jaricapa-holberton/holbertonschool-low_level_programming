b6d8c66a91d56b1e76a3efdc0f4df5ac2cb54119
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-09 09:26:41 -0500

    add v1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result
+ * @n: get a number
+ * @m: get a number
+ * Return: return 0
+ */
+int add(int n, int m)
+{
+	int o;
+
+	o = n + m;
+	return (o);
+}
