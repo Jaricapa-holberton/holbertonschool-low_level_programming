5f3dcab49491f8eea05c261fde9208ee405ae9b8
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-08 19:02:04 -0500

    islower v1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _islower - check for lowercases
+ * c: get a letter
+ * Return: return 1 if c is lowercases and 0 in otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 93 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
