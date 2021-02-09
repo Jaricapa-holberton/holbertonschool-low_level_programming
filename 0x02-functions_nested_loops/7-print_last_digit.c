04000f4c07868a607807031d726ec87e52282e98
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-09 06:43:56 -0500

    last digit v1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,23 @@
 #include "holberton.h"
 /**
- * main - function main balblabla
- * @var1: balbla
- * @var2: balblab
- *
- * Return: return balblabva
+ * print_last_digit - prints the last digit of a number
+ * @n: get a number
+ * Return: return the value of the last digit
  */
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+
+	if (m < 0)
+	{
+		_putchar((-1 * m) + '0');
+		return (-1 * m);
+	}
+	else
+	{
+		_putchar((m + '0'));
+		return (m);
+	}
+}
