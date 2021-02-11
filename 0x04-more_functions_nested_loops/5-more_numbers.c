fbd6a002fe82e6b92a3cec9219444f953fe98158
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-11 18:36:47 -0500

    more numbers v1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line
+ * Return: return void
+ */
+void more_numbers(void)
+{
+	int L, N, M;
+
+	for (L = '0'; L <= '9'; L++)
+	{
+		for (M = '0'; M <= '1'; M++)
+		{
+			for (N = '0'; N <= '9'; N++)
+			{
+				if ((M != '1') || (N <= '4'))
+				{
+					if (M != '0')
+					{
+						_putchar(M);
+					}
+					_putchar(N);					
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
