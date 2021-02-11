3aecd6d9ea9d4a687b9700c7379dcd058c2d555a
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-10 20:58:10 -0500

    100 times v2

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -12,4 +12,5 @@ void jack_bauer(void); /* prints every minute of the day of Jack Bauer, starting
 void times_table(void); /* prints the 9 times table, starting with 0 */
 int add(int, int); /* adds two integers and returns the result */
 void print_to_98(int n); /* prints all natural numbers from n to 98, followed by a new line */
+void print_times_table(int n); /* prints the n times table, starting with 0 */
 #endif
