5499be3b78d131f09975d28027f407ba6a121dbb
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-11 19:04:53 -0500

    print line v1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: get a number
+ * Return: return void
+ */
+void print_line(int n)
+{
+	int m;
+
+	if (n > 0)
+	{
+		for (m = 0; m <= n; m++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
