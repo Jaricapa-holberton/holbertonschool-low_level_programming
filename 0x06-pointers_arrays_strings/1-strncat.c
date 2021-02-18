617e46d6f26a67eb422867e9efca299b9f3cde8f
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-18 16:11:13 -0500

    0 and 1 v1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * length - count length of a string
+ * @dest: get a string
+ * Return: return the count
+ */
+
+int length(char *dest)
+{
+	return ((*dest != '\0') ? 1 + length(dest + 1) : 0);
+}
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: get a pointer
+ * @src: get a pointer
+ * @n: get a number
+ * Return: return dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, log = length(dest);
+	/* char *first = dest; */
+
+	for (i = log; (i - log) < n && *(dest + i) != '\0'; src++)
+	{
+		*(dest + i) = *src;
+	}
+	*(dest + i) = *src;
+	return (dest);
+}
