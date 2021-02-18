ceab094bdf3fead76512cd3e98ee6790b60c7414
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-18 09:44:28 -0500

    first push

---
+++ 0x05-pointers_arrays_strings/7-prueba.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half of a
+ * string, followed by a new line
+ * @str: get a string
+ * Return: return void
+ */
+void puts_half(char *str)
+{
+	int b, c;
+
+	b = 0;
+	while (str[b] != 0)
+	{
+		b++;
+	}
+	if ((b % 2) == 0)
+	{
+		c = (b / 2);
+		while (c <= b)
+		{
+			_putchar(*(str + c));
+			c++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		c = (b - 1) / 2;
+		c = b - c;
+		while (c <= b)
+		{
+			_putchar(*(str + c));
+			c++;
+		}
+		_putchar('\n');
+	}
+}
