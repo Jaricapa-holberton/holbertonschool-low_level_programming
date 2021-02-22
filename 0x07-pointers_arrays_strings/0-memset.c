c9b1fb0359871b1a0a1773289c750461e1465208
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-22 13:56:18 -0500

    memset v2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * main -  fills the first n bytes of 
+ * _memset -  fills the first n bytes of
  * the memory area pointed to by s with
  * the constant byte b
  * @s: get a array
