5129eb5121da6329e372c5e25675d5266ec1003c
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-22 21:01:36 -0500

    strspn v1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strspn - searches a string for any of a set of bytes
+ * @s: get a string
+ * @accept: get a string
+ * Return: return the number o bytes finded
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (i);
+}
