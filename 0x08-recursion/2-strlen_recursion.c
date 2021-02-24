7ef49b8bec21fe7684af02570f888a08e1d01606
Jaricapa-holberton <2241@holbertonschool.com>
2021-02-24 18:53:26 -0500

    strlen v1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: get a string
+ * Return: return a number
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s != '\0')
+	{
+		length = _strlen_recursion(s + 1) + 1;
+	}
+	return (length);
+}
