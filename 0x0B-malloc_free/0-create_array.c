bc03ad6fa5403dd5bf16d604c016392389a7b3c3
Jaricapa-holberton <2241@holbertonschool.com>
2021-03-02 16:01:32 -0500

    create array v4

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	if (size != 0)
 	{
 		for (i = 0; i < size; i++)
