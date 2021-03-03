5170cc5b0f2d8734a4a694c77fa3230b252244ee
Jaricapa-holberton <2241@holbertonschool.com>
2021-03-03 13:09:46 -0500

    alloc v5

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,13 +14,17 @@ int **alloc_grid(int width, int height)
 	p = NULL;
 
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
-
+	}
+/*Create a pointer to pointer rows*/
 	p = malloc(sizeof(int *) * height);
 
 	if (p == NULL)
+	{
 		return (NULL);
-
+	}
+/*For each row create a pointer to each element*/
 	for (l = 0; l < height; l++)
 	{
 		p[l] = malloc(sizeof(int) * width);
@@ -28,15 +32,20 @@ int **alloc_grid(int width, int height)
 		if (p[l] == NULL)
 		{
 			for (p[l] -= 1; l >= 0; l--)
+			{
 				free(p[l]);
+			}
 			free(p);
 			return (NULL);
 		}
 	}
-
+/*Fill the array*/
 	for (l = 0; l < height; l++)
+	{
 		for (c = 0; c < width; c++)
+		{
 			p[l][c] = 0;
-
+		}
+	}
 	return (p);
 }
