1fbc70653ca474009a6f425361ba506b502fffbb
Jaricapa-holberton <2241@holbertonschool.com>
2021-03-03 12:18:37 -0500

    alloc v1

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * alloc_grid - Return a pointer to a 2 dimensional array of integers.
+ * @width: Columns.
+ * @height: Rows.
+ * Return: Pointer to a 2 dimensional array.
+ */
+int **alloc_grid(int width, int height)
+{
+	int fil, col;
+	int **p;
+
+	/* create a pointer to pointer rows */
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	p = (int **) malloc(sizeof(int *) * height);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	/* for each row create a pointer to each element */
+	for (fil = 0; fil < height; fil++)
+	{
+		p[fil] = (int *) malloc(sizeof(int) * width);
+		if (p[fil] == NULL)
+		{
+			for (col = fil - 1; col >= 0; col--)
+			{
+				free(p[fil]);
+			}
+			free(p);
+			return (NULL);
+		}
+		/* fill the array with zeros */
+		for (col = 0; col < width ; col++)
+		{
+			p[fil][col] = 0;
+		}
+	}
+	return (p);
+}
