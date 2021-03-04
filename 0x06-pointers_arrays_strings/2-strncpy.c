18a9213e746acdb0abec2edb91a1e9ba907a2e06
Jaricapa-holberton <70967240+Jaricapa-holberton@users.noreply.github.com>
2021-03-04 14:41:03 -0500

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,31 +1,20 @@
-#include  "holberton.h"
+#include "holberton.h"
+
 /**
- * _strncpy - copies the string pointed to
- * by src, including the terminating null
- * byte (\0), to the buffer pointed to by dest
- * @dest: get a array
- * @src: get a array
- * @n: get a number
- * Return: return dest
+ * *_strncpy - Copy n bytes from source to destiny string.
+ * @dest: Destiny string.
+ * @src: Source string.
+ * @n: Bytes from src string.
+ * Return: Destiny string dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
 
-	i = 0;
-	while (*src != '\0' && i < n)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		i++;
-	}
-	for (j = i; j < n; j++)
-	{
-		*dest = '\0';
-		dest++;
-	}
-	dest = dest - j;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
