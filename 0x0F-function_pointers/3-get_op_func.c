167b9f971726faba557f4d72dc2042bfad2b0074
Jaricapa-holberton <2241@holbertonschool.com>
2021-03-10 20:22:33 -0500

    fixed error 99

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,14 +20,12 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s)
+		if (s == ops[i].op && s[1] == '\0')
 		{
 			return (ops[i].f);
 		}
 		i++;
 	}
-
-	printf("Error\n");
-	exit(99);
+	return (NULL);
 }
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,22 +8,29 @@
 int main(int argc, char **argv)
 {
 	int num1, num2, res;
-	char *operator;
 	int (*calculation)(int, int);
 
+	/* check error 98*/
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	/* Usage: calc num1 operator num2 */
-	num1 = atoi(argv[1]);
-	operator = argv[2];
-	calculation = get_op_func(operator);
+
+	/* check error 99 */
+	calculation = get_op_func(argv[2]);
+	if (calculation == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+		
+	}
 	num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
 	res = calculation(num1, num2);
 
+	
 	printf("%d\n", res);
 	return (0);
 }
