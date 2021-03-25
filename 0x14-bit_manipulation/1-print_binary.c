9b7856add2dddff940789c068bd8e9ea90246189
Jaricapa-holberton <2241@holbertonschool.com>
2021-03-25 17:50:28 -0500

    fixed variable names

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,14 +5,16 @@
  */
 void get_binary(unsigned long int n)
 {
-	short int bn; /*Binary number (bit)*/
+	short int binary; /*get if the number is 1 in binary */
 
 	if (n != 0)
 	{
-		/*Enter the function by removing the last bit*/
+		/* Enter the function by removing the last bit */
 		get_binary(n >> 1);
-		bn = n & 1;
-		_putchar(bn + '0');
+		/* compare if its 1 */
+		binary = n & 1;
+		/* show the binary */
+		_putchar(binary + '0');
 	}
 }
 /**
@@ -25,6 +27,7 @@ void print_binary(unsigned long int n)
 	{
 		get_binary(n);
 	}
+	/* if the binary is not 1, then just print 0 */
 	else
 	{
 		_putchar('0');
