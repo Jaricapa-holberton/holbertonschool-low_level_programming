d52f610a69b2eb7f827ff2b8d110821e2bacaea9
Jaricapa-holberton <2241@holbertonschool.com>
2021-04-30 18:52:21 -0500

    task 5 created

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,27 +1,27 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - Returns the node at the requested index.
+ * get_dnodeint_at_index - Returns the node at the requested index.
  * @head: The list's head.
  * @index: The index of the requested node.
  * Return: The node at the requested index.
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	/* create a counter to compare the position vs index */
 	unsigned int counter;
 	/* create a aux node to save the direction of the next node */
-	listint_t *cnode;
+	dlistint_t *cnode;
 	/* create a copy of head to not lose the head node */
 	cnode = head;
-	/* run trought the list until the n position is found */
-	for (counter = 0; counter != index; counter++)
+	/* run trought a node dont exists */
+	for (counter = 0; cnode; counter++)
 	{
 		/* check if the node exists */
-		if (cnode == NULL)
+		if (counter == index)
 		{
-			return (NULL);
+			return (cnode);
 		}
 		cnode = cnode->next;
 	}
-	return (cnode);
+	return (NULL);
 }
