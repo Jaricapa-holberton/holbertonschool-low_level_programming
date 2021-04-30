28a6f6715032c90768afebb674907dc1823efe32
Jaricapa-holberton <2241@holbertonschool.com>
2021-04-30 18:54:42 -0500

    task 6 created

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 /**
- * sum_listint - Returns the sum of the elements of all the nodes.
+ * sum_dlistint - Returns the sum of the elements of all the nodes.
  * @head: The list's head.
  *
  * Return: The sum of the elements of all the nodes.
  */
-int sum_listint(listint_t *head)
+int sum_dlistint(dlistint_t *head)
 {
 	/* create a aux var to save the sum */
 	int sum;
 	/* create a aux node to save the directions */
-	listint_t *cnode;
+	dlistint_t *cnode;
 
 	/* asing the direction of head to the aux node */
 	cnode = head;
 	/* run trough the list to get the sum */
-	for (sum = 0; cnode != NULL;)
+	for (sum = 0; cnode;)
 	{
 		/* add the value on n to sum */
 		sum += cnode->n;
