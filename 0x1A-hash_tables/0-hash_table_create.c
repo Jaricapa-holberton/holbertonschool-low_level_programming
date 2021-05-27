e918caf78733dd6842f57835104aab561cd453fc
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-27 15:29:53 -0500

    task 0 v1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: get a number
+ * Return: return a pointer or null if fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable = NULL;
+	unsigned long int i; /* to iterate the array */
+
+	/* create hash table */
+	hashtable = malloc(sizeof(hash_table_t));
+	if (!hashtable)
+		return (NULL);
+
+	/* now, create the array to save the data */
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hashtable->array)
+		return (NULL);
+
+	/* finnaly, initialize the array whith all elements as null */
+	for (i = 0; i < size; i++)
+		hashtable->array[i] = NULL;
+	/* done */
+	return (hashtable);
+}
